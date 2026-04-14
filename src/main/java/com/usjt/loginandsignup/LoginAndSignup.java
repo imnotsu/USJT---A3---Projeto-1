1bde52c1d98a5be02f8004322b0f78e34c4b4345
Su <imnotsu@proton.me>
2026-04-13 23:43:42 -0300

    Add Login & Signup Beta Template (W.I.P)

---
+++ src/main/java/com/usjt/loginandsignup/LoginAndSignup.java
@@ -0,0 +1,11 @@
+package com.usjt.loginandsignup;
+
+public class LoginAndSignup {
+
+    public static void main(String[] args) {
+        Login LoginFrame = new Login();
+        LoginFrame.pack();
+        LoginFrame.setVisible(true);
+        LoginFrame.setLocationRelativeTo(null); // centro
+    }
+}
